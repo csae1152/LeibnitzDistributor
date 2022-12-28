589e27b7119d84e00949233271a03446f29bc261
Helmut Steiner <helmut.steinersti2@gmail.com>
2022-12-28 12:43:48 +0100

    initialize

---
+++ src/main/java/com/leibnitz/leibnitzdistributor/LeibnitzDistributor.java
@@ -0,0 +1,12 @@
+package com.leibnitz.leibnitzdistributor;
+
+/**
+ *
+ * @author helmut
+ */
+public class LeibnitzDistributor {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
